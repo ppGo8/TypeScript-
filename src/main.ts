c7dd0aed076091dc9f8c630e4d7c0718da446d0c
ppgo8 <1159316682@qq.com>
2023-11-21 23:17:14 +0800

    搭建界面基本样式

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 // ts规定:导入ts文件不需要写后缀
 // webpack规定: 处理未写后缀名的内容会:.js ==> .json 都没有报错
 // 因此需要补充webpack配置, 未写后缀名时如何操作
-import a from './a';
-import './a.css';
+import './main.css';
 
-// let a: string = 'hello';
-console.info(a);
+const listItem = document.querySelectorAll('#list li');
+
+for (let i=0; i < listItem.length; i++) {
+  listItem[i].addEventListener('click', () => {
+    let url = (listItem[i] as HTMLElement).dataset.url;
+    let title = (listItem[i] as HTMLElement).dataset.title;
+    console.info(url, title);
+  })
+}
